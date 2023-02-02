4391a8b26cf673d65fbd977464e4c11067de2b8a
Sergio Hernandez-Acosta <79073281+sergiojhernandezacosta@users.noreply.github.com>
2023-02-02 10:24:32 -0600

    Update movieloop.py

--- movieloop.py
+++ movieloop.py
@@ -18,9 +18,10 @@ while True:
     # If the user enters 'quit', breaking out of the loop
     if movie == 'quit':
         break
+        
     # Prompting the user for the information they want to retrieve
-    key = input("Enter the info you want to retrieve (e.g. Director, Writer, Year, etc.): ")
+    key = input("\nEnter the info you want to retrieve (e.g. Director, Writer, Year, etc.): ")
     # Getting the requested information by calling the get_movie_info function with the movie title and key
     result = get_movie_info(movie, key)
     # Printing the result to the console
-    print(f'The {key} of {movie} is {result}')
+    print(f'\nThe {key} of {movie} is {result}\n')
